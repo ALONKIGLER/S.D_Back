c4b5c9e0b76b0ade122f861bab3a9dfc19d46277
Alon Kigler <kiglerr@gmail.com>
2023-05-22 20:06:14 +0300

    ch

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello,the service is live 🎉!");
 });
 
 app.listen(3000, () => {
